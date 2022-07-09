a605818409ceaacc9fbd3771eca56093a8fb371f
Your Name <samadhinilakshana1999@gmail.com>
2022-07-09 18:49:56 +0530

    Update Content

--- Back_End/src/main/java/lk/easyrental/controller/AdminController.java
+++ Back_End/src/main/java/lk/easyrental/controller/AdminController.java
@@ -1,11 +1,13 @@
 package lk.easyrental.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
-@RequestMapping("admin")
+@RequestMapping("api/v1/admin")
 public class AdminController {
 
     @GetMapping
