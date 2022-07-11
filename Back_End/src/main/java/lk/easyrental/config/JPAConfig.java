9121e759477ea513227129185f0ca01545212962
Your Name <samadhinilakshana1999@gmail.com>
2022-07-11 18:12:13 +0530

    Added Hibernate

--- Back_End/src/main/java/lk/easyrental/config/JPAConfig.java
+++ Back_End/src/main/java/lk/easyrental/config/JPAConfig.java
@@ -1,4 +1,16 @@
 package lk.easyrental.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+
+@Configuration
 public class JPAConfig {
+
+//    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(){
+//        LocalContainerEntityManagerFactoryBean bean=new LocalContainerEntityManagerFactoryBean();
+//        bean.setJpaVendorAdapter();
+//        bean.setDataSource();
+//        return bean;
+//    }
 }
