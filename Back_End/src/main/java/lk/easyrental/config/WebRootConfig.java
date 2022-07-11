7b4b46d89506cb0824387dbad079b6042da1bee3
Your Name <samadhinilakshana1999@gmail.com>
2022-07-11 17:37:41 +0530

    Import JPA Config to the WebRoot Config

--- Back_End/src/main/java/lk/easyrental/config/WebRootConfig.java
+++ Back_End/src/main/java/lk/easyrental/config/WebRootConfig.java
@@ -1,7 +1,9 @@
 package lk.easyrental.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(JPAConfig.class)
 public class WebRootConfig {
 }
