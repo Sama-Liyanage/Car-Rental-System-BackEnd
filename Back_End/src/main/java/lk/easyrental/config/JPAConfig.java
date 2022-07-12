2c3cd41e3f676bb708d9f97bcd961eba9a32a3e9
Your Name <samadhinilakshana1999@gmail.com>
2022-07-12 21:52:25 +0530

    Added property file

--- Back_End/src/main/java/lk/easyrental/config/JPAConfig.java
+++ Back_End/src/main/java/lk/easyrental/config/JPAConfig.java
@@ -3,9 +3,12 @@ package lk.easyrental.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -14,11 +17,12 @@ public class JPAConfig {
 
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(){
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(DataSource ds, JpaVendorAdapter va){
         LocalContainerEntityManagerFactoryBean bean=new LocalContainerEntityManagerFactoryBean();
-        bean.setJpaVendorAdapter();
-        bean.setDataSource();
+        bean.setJpaVendorAdapter(va);
+        bean.setDataSource(ds);
+        bean.setPackagesToScan("lk.easyrental.entity");
         return bean;
-
     }
+    
 }
