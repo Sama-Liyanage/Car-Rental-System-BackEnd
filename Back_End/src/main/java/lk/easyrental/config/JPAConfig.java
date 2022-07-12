0c81568cc61f841a1586afae640bd32c6014c3d5
Your Name <samadhinilakshana1999@gmail.com>
2022-07-12 20:34:09 +0530

    Update

--- Back_End/src/main/java/lk/easyrental/config/JPAConfig.java
+++ Back_End/src/main/java/lk/easyrental/config/JPAConfig.java
@@ -1,16 +1,24 @@
 package lk.easyrental.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class JPAConfig {
 
-//    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(){
-//        LocalContainerEntityManagerFactoryBean bean=new LocalContainerEntityManagerFactoryBean();
-//        bean.setJpaVendorAdapter();
-//        bean.setDataSource();
-//        return bean;
-//    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(){
+        LocalContainerEntityManagerFactoryBean bean=new LocalContainerEntityManagerFactoryBean();
+        bean.setJpaVendorAdapter();
+        bean.setDataSource();
+        return bean;
+
+    }
 }
