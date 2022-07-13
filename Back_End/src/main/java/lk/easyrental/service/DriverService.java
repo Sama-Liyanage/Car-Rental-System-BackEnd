210148d088dae0a1233845a633a0c098ef6893ec
Your Name <samadhinilakshana1999@gmail.com>
2022-07-13 22:15:10 +0530

    Update Content

--- Back_End/src/main/java/lk/easyrental/service/DriverService.java
+++ Back_End/src/main/java/lk/easyrental/service/DriverService.java
@@ -1,4 +1,13 @@
 package lk.easyrental.service;
 
+import lk.easyrental.dto.DriverDTO;
+
+import java.util.List;
+
 public interface DriverService {
+    void saveDriver(DriverDTO dto);
+    void updateDriver(DriverDTO dto);
+    void deleteDriver(String id);
+    DriverDTO searchDriver(String id);
+    List<DriverDTO> getAllDrivers();
 }
--- Back_End/src/main/java/lk/easyrental/service/impl/DriverServiceImpl.java
+++ Back_End/src/main/java/lk/easyrental/service/impl/DriverServiceImpl.java
@@ -1,11 +1,68 @@
 package lk.easyrental.service.impl;
 
+import lk.easyrental.dto.DriverDTO;
+import lk.easyrental.entity.Driver;
+import lk.easyrental.repo.DriverRepo;
 import lk.easyrental.service.DriverService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
 public class DriverServiceImpl implements DriverService {
+
+    @Autowired
+    ModelMapper mapper;
+
+    @Autowired
+    DriverRepo driverRepo;
+
+    @Override
+    public void saveDriver(DriverDTO dto) {
+        if (!driverRepo.existsById(dto.getDriverID())){
+            driverRepo.save(mapper.map(dto, Driver.class));
+        }else{
+            throw new RuntimeException("Driver Already Exists! Try Again");
+        }
+    }
+
+    @Override
+    public void deleteDriver(String id) {
+        if (driverRepo.existsById(id)){
+            driverRepo.deleteById(id);
+        }else{
+            throw new RuntimeException("Driver Not Exist..! Please Check ID");
+        }
+    }
+
+    @Override
+    public void updateDriver(DriverDTO dto) {
+        if (driverRepo.existsById(dto.getDriverID())){
+            driverRepo.save(mapper.map(dto,Driver.class));
+        }else{
+            throw new RuntimeException("Driver Not Exist..! Please Check ID");
+        }
+    }
+
+    @Override
+    public DriverDTO searchDriver(String id) {
+        if (driverRepo.existsById(id)) {
+            return mapper.map(driverRepo.findById(id).get(), DriverDTO.class);
+        }else{
+            throw new RuntimeException("No Driver For " + id + " ..!");
+        }
+    }
+
+    @Override
+    public List<DriverDTO> getAllDrivers() {
+        return mapper.map(driverRepo.findAll(), new TypeToken<List<DriverDTO>>() {
+        }.getType());
+    }
+
+
 }
