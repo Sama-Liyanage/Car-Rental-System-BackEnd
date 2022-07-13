c05920a827b4dc4e4562607fbb0cf21f88e50d67
Your Name <samadhinilakshana1999@gmail.com>
2022-07-13 22:34:29 +0530

    Added Login Controller

--- Back_End/src/main/java/lk/easyrental/controller/LoginControlller.java
+++ Back_End/src/main/java/lk/easyrental/controller/LoginControlller.java
@@ -1,4 +1,29 @@
 package lk.easyrental.controller;
 
+
+import lk.easyrental.dto.LoginDTO;
+import lk.easyrental.service.LoginService;
+import lk.easyrental.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("api/v1/login")
 public class LoginControlller {
+
+    @Autowired
+    LoginService loginService;
+
+    @ResponseStatus(HttpStatus.CREATED)//201
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveLogin(@RequestBody LoginDTO login){
+        loginService.saveLogin(login);
+        return new ResponseUtil(200,"Saved",null);
+    }
+
+
 }
+
