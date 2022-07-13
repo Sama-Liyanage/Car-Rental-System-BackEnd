ad986fdb24dab36dcae0e85471aa0dba67c15879
Your Name <samadhinilakshana1999@gmail.com>
2022-07-13 21:53:56 +0530

    Complete Car Controller

--- Back_End/src/main/java/lk/easyrental/controller/CarController.java
+++ Back_End/src/main/java/lk/easyrental/controller/CarController.java
@@ -1,4 +1,48 @@
 package lk.easyrental.controller;
 
+
+import lk.easyrental.dto.CarDTO;
+import lk.easyrental.service.CarService;
+import lk.easyrental.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("api/v1/car")
 public class CarController {
+
+    @Autowired
+    CarService carService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllCars(){
+        return new ResponseUtil(200,"Ok",carService.getAllCars());
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)//201
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveCar(@RequestBody CarDTO car){
+        carService.saveCar(car);
+        return new ResponseUtil(200,"Saved",null);
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil updateCar(@RequestBody CarDTO car){
+        carService.updateCar(car);
+        return new ResponseUtil(200,"Updated",null);
+    }
+
+    @DeleteMapping(path = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteCar(@PathVariable String id){
+        carService.deleteCar(id);
+        return new ResponseUtil(200,"Deleted",null);
+    }
+
+    @GetMapping(path = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil searchCar(@PathVariable String id){
+        return new ResponseUtil(200,"Ok",carService.searchCar(id));
+    }
 }
