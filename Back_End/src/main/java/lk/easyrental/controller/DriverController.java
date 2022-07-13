413594f3f3eb7f0c758f9275b9be1a1eb988904f
Your Name <samadhinilakshana1999@gmail.com>
2022-07-13 22:22:28 +0530

    Added DriverController

--- Back_End/src/main/java/lk/easyrental/controller/DriverController.java
+++ Back_End/src/main/java/lk/easyrental/controller/DriverController.java
@@ -1,4 +1,50 @@
 package lk.easyrental.controller;
 
+
+import lk.easyrental.dto.DriverDTO;
+import lk.easyrental.service.DriverService;
+import lk.easyrental.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("api/v1/driver")
 public class DriverController {
+
+    @Autowired
+    DriverService driverService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllDrivers(){
+        return new ResponseUtil(200,"Ok",driverService.getAllDrivers());
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)//201
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveDriver(@RequestBody DriverDTO driver){
+        driverService.saveDriver(driver);
+        return new ResponseUtil(200,"Saved",null);
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil updateDriver(@RequestBody DriverDTO driver){
+        driverService.updateDriver(driver);
+        return new ResponseUtil(200,"Updated",null);
+    }
+
+    @DeleteMapping(path = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteDriver(@PathVariable String id){
+        driverService.deleteDriver(id);
+        return new ResponseUtil(200,"Deleted",null);
+    }
+
+    @GetMapping(path = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil searchDriver(@PathVariable String id){
+        return new ResponseUtil(200,"Ok",driverService.searchDriver(id));
+    }
+
+
 }
